1798eb686ec5731b014d51711b1e7a1531213706
Caseypatel <keshidiva@gmail.com>
2024-10-30 16:25:35 -0400

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_Os6smkdK2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3cktjrfmd9fbmuttrmqae2m099', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://p6asr6y70f.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
